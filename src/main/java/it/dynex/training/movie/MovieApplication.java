c2d896b4c0e9504854cf96e047905318e57f436b
Daniel <dllz@users.noreply.github.com>
2018-11-03 10:30:03 +0200

    Initial Commit

---
+++ src/main/java/it/dynex/training/movie/MovieApplication.java
@@ -0,0 +1,14 @@
+package it.dynex.training.movie;
+
+import com.thedeanda.lorem.Lorem;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApplication.class, args);
+	}
+}
---
+++ src/main/java/it/dynex/training/movie/config/CacheConfig.java
@@ -0,0 +1,29 @@
+package it.dynex.training.movie.config;
+
+import com.google.common.cache.CacheBuilder;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+	public static final String CONTENT_CACHE = "CONTENT_CACHE";
+
+	@Bean
+	public CacheManager cacheManager() {
+		SimpleCacheManager cacheManager = new SimpleCacheManager();
+		cacheManager.setCaches(Arrays.asList(
+				new ConcurrentMapCache(CONTENT_CACHE, CacheBuilder.newBuilder().expireAfterWrite(30, TimeUnit.SECONDS).build().asMap(), false)));
+		return cacheManager;
+	}
+
+
+}
